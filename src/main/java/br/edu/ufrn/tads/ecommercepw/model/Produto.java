0e7f35c5dcb281ef07c19e0e2ab4695d538e1f18
Pierre Brito <carlospierre07@gmail.com>
2025-04-22 13:33:15 -0300

    models

---
+++ src/main/java/br/edu/ufrn/tads/ecommercepw/model/Produto.java
@@ -0,0 +1,79 @@
+package br.edu.ufrn.tads.ecommercepw.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private double preco;
+
+    private int quantidade; // estoque
+
+    public Produto() {
+    }
+
+    public Produto(String nome, String descricao, double preco, int quantidade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void diminuiEstoque(int quantidadeComprada) {
+        this.quantidade -= quantidadeComprada;
+    }
+
+    public void incrementaEstoque(int quantidade) {
+        this.quantidade += quantidade;
+    }
+}
+
