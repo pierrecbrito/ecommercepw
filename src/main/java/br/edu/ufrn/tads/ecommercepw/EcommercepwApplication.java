fad479c40f64d17cfe241c5a95c30fecf793d8b4
Pierre Brito <carlospierre07@gmail.com>
2025-04-22 13:23:22 -0300

    first commit

---
+++ src/main/java/br/edu/ufrn/tads/ecommercepw/EcommercepwApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.tads.ecommercepw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercepwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercepwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufrn/tads/ecommercepw/EcommercepwApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.tads.ecommercepw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercepwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
