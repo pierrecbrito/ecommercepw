179846de9aae1acca6a4dc99cb1fcd1004638172
Pierre Brito <carlospierre07@gmail.com>
2025-05-01 00:36:02 -0300

    Database Connection

---
+++ src/main/java/br/edu/ufrn/tads/ecommercepw/connection/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package br.edu.ufrn.tads.ecommercepw.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/ecommerce";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL JDBC Driver not found", e);
+        }
+    }
+}
